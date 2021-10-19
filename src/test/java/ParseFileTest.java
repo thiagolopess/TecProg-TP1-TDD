7c69c3fd2fcfa1a5c05b3dceba2695d6a0360feb
thiagolopess <thiago.lopes.santos.tls@gmail.com>
2021-10-19 20:41:27 -0300

    Obtain red bar by lack of implementation at ParseFileTest
    Co-authored-by: igor-paiva <igorbatistapaiva@outlook.com>
    Co-Authored-by: Arthur <arthur001123@gmail.com>

--- src/test/java/ParseFileTest.java
+++ src/test/java/ParseFileTest.java
@@ -20,4 +20,21 @@ public class ParseFileTest {
         Assert.assertArrayEquals(parser.fileData.get(1), new int[] { 2 });
         Assert.assertArrayEquals(parser.fileData.get(2), new int[] { 3 });
     }
+
+    @Test
+    public void test1() {
+        FileParser parser = new FileParser();
+
+        try {
+            parser.readFile("src/test/resources/analysisMemory02.out");
+        } catch (ArquivoNaoEncontradoException e) {
+            e.printStackTrace();
+        }
+
+        parser.parseDataFile();
+
+        Assert.assertArrayEquals(parser.fileData.get(0), new int[] { 1, 3, 4 });
+        Assert.assertArrayEquals(parser.fileData.get(1), new int[] { 2, 2, 3 });
+        Assert.assertArrayEquals(parser.fileData.get(2), new int[] { 4, 1, 2 });
+    }
 }
