2f1cb54280776d61d48c9ebe5d794cf680ecb1c8
thiagolopess <thiago.lopes.santos.tls@gmail.com>
2021-10-20 23:52:18 -0300

    Add implementation of main
    Co-authored-by: igor-paiva <igorbatistapaiva@outlook.com>
    Co-Authored-by: Arthur <arthur001123@gmail.com>

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,25 +1,26 @@
 package app;
 
-
 public class Main {
-
     public static void main(String[] args) {
-
         try {
-
             FileParser parser = new FileParser();
 
             parser.readFile("resources/analysisMemory.out");
 
             parser.parseDataFile();
 
-            parser.writeOutputFile("linhas", "resources/analysisMemoryTab.out");
+            System.out.print("Digite o delimitador: ");
+            parser.readOutputDelimiter();
 
+            System.out.print("\nDigite o formato (linhas ou colunas): ");
+            parser.readOutputFormat();
 
+            System.out.print("\nDigite o caminho do arquivo de saída: ");
+            parser.readOutputPath();
+
+            parser.writeOutputFile();
         } catch(Exception e) {
             e.printStackTrace();
         }
-
-        // write your code here
     }
 }
